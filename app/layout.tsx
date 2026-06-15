6cafe769a8ef8232c81a545e28544e40f3590315
Kristjan <draksler.kristjan@gmail.com>
2026-06-15 15:10:40 +0200

    Dodaj Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
 import CookieBanner from "@/components/cookie-banner";
+import Script from "next/script";
 import "./globals.css";
 
 const geist = Geist({
@@ -22,6 +23,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="sl" suppressHydrationWarning className={`${geist.variable} h-full antialiased`}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-N4TRKZKDCR"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-N4TRKZKDCR');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col">
         <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
           {children}
